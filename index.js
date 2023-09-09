df28adbd9e853a5811976aca2133e5b8db3cf126
josephDorsey <jmdorsey92@gmail.com>
2023-09-09 11:24:59 -0700

    Add functionality to start and reset game buttons

--- index.js
+++ index.js
@@ -5,12 +5,53 @@ const userNameText = document.querySelector(".game-user-name-text");
 const buttonSaveUserName = document.querySelector(".game-board-button");
 
 const markerX = document.querySelector(".marker-x");
-const markerY = document.querySelector(".marker-y");
+const markerO = document.querySelector(".marker-o");
 
 const moveCounterTitle = document.querySelector(".move-count-title");
 
 const gameBoard = document.querySelector(".game-board");
 const gameBoardPlayerTitle = document.querySelector(".game-board-player-title");
+const gameBoardMarkerContainer = document.querySelector(
+  ".game-board-marker-container"
+);
 
 const buttonStartGame = document.querySelector(".game-start");
 const buttonResetGame = document.querySelector(".game-reset");
+
+function playerSelectMarker(symbol) {
+  let newSymbol = symbol.toLowerCase();
+  if (newSymbol === "x") {
+    markerX.classList.add("selected-marker");
+    markerO.classList.remove("selected-marker");
+  } else {
+    markerX.classList.remove("selected-marker");
+    markerO.classList.add("selected-marker");
+  }
+}
+function startGame() {
+  moveCounterTitle.style.display = "flex";
+  gameBoardPlayerTitle.style.display = "flex";
+}
+
+function resetGame() {
+  moveCounterTitle.style.display = "none";
+  gameBoardPlayerTitle.style.display = "none";
+  markerX.classList.remove("selected-marker");
+  markerO.classList.remove("selected-marker");
+}
+
+// Buttons
+gameBoardMarkerContainer.addEventListener("click", function (e) {
+  if (e.target.name === "x") playerSelectMarker("x");
+  if (e.target.name === "o") playerSelectMarker("o");
+});
+
+buttonSaveUserName.addEventListener("click", function (e) {
+  e.preventDefault();
+  userNameText.textContent = userName.value;
+  userName.value = "";
+  userName.classList.add("hidden");
+});
+
+buttonStartGame.addEventListener("click", startGame);
+buttonResetGame.addEventListener("click", resetGame);
