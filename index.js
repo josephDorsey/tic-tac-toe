eff3fef7162d4eb77f69ee500d072119aecc8ac5
josephDorsey <jmdorsey92@gmail.com>
2023-09-08 20:14:57 -0700

    init commit  🚀

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const userName = document.querySelector(".game-user-name");
+const userNameText = document.querySelector(".game-user-name-text");
+const buttonSaveUserName = document.querySelector(".game-board-button");
+
+const markerX = document.querySelector(".marker-x");
+const markerY = document.querySelector(".marker-y");
+
+const moveCounterTitle = document.querySelector(".move-count-title");
+
+const gameBoard = document.querySelector(".game-board");
+const gameBoardPlayerTitle = document.querySelector(".game-board-player-title");
+
+const buttonStartGame = document.querySelector(".game-start");
+const buttonResetGame = document.querySelector(".game-reset");
